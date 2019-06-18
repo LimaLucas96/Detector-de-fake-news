ea3be4163f86419356f5ff428e435320dde18eb6
Lucas Lima <l.lima19@hotmail.com>
2019-06-18 19:41:29 -0300

    Update

--- src/br/imd/ufrn/lpii/app/principal.java
+++ src/br/imd/ufrn/lpii/app/principal.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class principal extends Application {
+public class Principal extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
